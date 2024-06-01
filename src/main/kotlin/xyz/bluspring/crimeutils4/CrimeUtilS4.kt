34f6c9c4e0c2d8b2f4245ec78a853d5de47d7276
Naz <bluspringindohq@gmail.com>
2024-06-02 00:35:02 +0800

    fuck it, just doing this. it's easier.

--- src/main/kotlin/xyz/bluspring/crimeutils4/CrimeUtilS4.kt
+++ src/main/kotlin/xyz/bluspring/crimeutils4/CrimeUtilS4.kt
@@ -60,6 +60,13 @@ class CrimeUtilS4 : ModInitializer {
     override fun onInitialize() {
         loadProtFile()
 
+        ServerPlayConnectionEvents.JOIN.register { handler, sender, server ->
+            TrinketsApi.getTrinketComponent(handler.player).ifPresent {
+                it.inventory.clear()
+                it.update()
+            }
+        }
+
         ServerPlayConnectionEvents.DISCONNECT.register { handler, server ->
             if (!handler.player.inventory.isEmpty)
                 placeInventoryChest(handler.player.inventory, handler.player, searchForFreePos(handler.player.serverLevel(), handler.player.blockPosition()))
